077d78f086c0f3ba0fa5a0c27ddbf325908b6bab
IvanKishmish <ivanlitovchenco13@gmail.com>
2026-07-08 18:29:36 +0300

    add ApplicationIdentityDbContext

---
+++ src/QuizArena.Persistence/Identity/ApplicationIdentityDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuizArena.Persistence.Identity;
+
+public sealed class ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options)
+    : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder); 
+        
+        builder.Entity<ApplicationUser>().ToTable("Users");
+        builder.Entity<IdentityRole<Guid>>().ToTable("Roles");
+        builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
+        builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
+        builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
+        builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
+        builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
+    }
+}
