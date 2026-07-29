b0545c7bd784f8a096881dadc4258e8438fcd8c7
IvanKishmish <ivanlitovchenco13@gmail.com>
2026-07-29 13:13:46 +0300

    refactor: extract configuration keys into constants

---
+++ src/backend/QuizArena.Persistence/ConfigurationKeys.cs
@@ -0,0 +1,9 @@
+namespace QuizArena.Persistence;
+
+internal static class ConfigurationKeys
+{
+    public const string DbConnectionString = "DB_CONNECTION_STRING";
+    public const string MongoConnectionString = "Mongo:ConnectionString";
+    public const string MongoDatabaseName = "Mongo:DatabaseName";
+    public const string RedisConnectionString = "Redis:ConnectionString";
+}
