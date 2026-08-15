f1164f6b9841dea3b638936b5181f1dd82db20d2
nico3331 <damegame955@gmail.com>
2026-08-15 17:13:38 +0200

    feat(frontend): add question type support

--- src/frontend/src/pages/QuizDetailsPage.tsx
+++ src/frontend/src/pages/QuizDetailsPage.tsx
@@ -43,7 +43,20 @@ function QuizDetailsPage({ accessToken }: QuizDetailsPageProps) {
     const [option3, setOption3] = useState("");
     const [option4, setOption4] = useState("");
     const [correctOption, setCorrectOption] = useState(0);
+    const [correctOptions, setCorrectOptions] = useState([
+        false,
+        false,
+        false,
+        false
+    ]);
     const navigate = useNavigate();
+
+    const [dataForm, setDataForm] = useState(false);
+
+    const [showHint, setShowHint] = useState(false);
+    const [questionType, setQuestionType] = useState(0);
+
+
 
     function handleTitleChange(event: ChangeEvent<HTMLInputElement>) {
         setTitle(event.target.value);
@@ -72,28 +85,99 @@ function QuizDetailsPage({ accessToken }: QuizDetailsPageProps) {
             return;
         }
 
-        const options = [
-            {
-                text: option1,
-                isCorrect: correctOption === 0,
-                orderIndex: 0
-            },
-            {
-                text: option2,
-                isCorrect: correctOption === 1,
-                orderIndex: 1
-            },
-            {
-                text: option3,
-                isCorrect: correctOption === 2,
-                orderIndex: 2
-            },
-            {
-                text: option4,
-                isCorrect: correctOption === 3,
-                orderIndex: 3
-            }
-        ];
+        let options;
+        if(questionType === 0){
+            options = [
+                {
+                    text: option1,
+                    isCorrect: correctOption === 0,
+                    orderIndex: 0
+                },
+                {
+                    text: option2,
+                    isCorrect: correctOption === 1,
+                    orderIndex: 1
+                },
+                {
+                    text: option3,
+                    isCorrect: correctOption === 2,
+                    orderIndex: 2
+                },
+                {
+                    text: option4,
+                    isCorrect: correctOption === 3,
+                    orderIndex: 3
+                }
+            ];
+            console.log("questionType:", questionType);
+            console.log("options:", options);
+        } else if(questionType === 1){
+            options = [
+                {
+                    text: option1,
+                    isCorrect: correctOptions[0],
+                    orderIndex: 0
+                },
+                {
+                    text: option2,
+                    isCorrect: correctOptions[1],
+                    orderIndex: 1
+                },
+                {
+                    text: option3,
+                    isCorrect: correctOptions[2],
+                    orderIndex: 2
+                },
+                {
+                    text: option4,
+                    isCorrect: correctOptions[3],
+                    orderIndex: 3
+                }
+            ];
+            console.log("questionType:", questionType);
+            console.log("options:", options);
+        } else if(questionType === 2){
+            options = [
+                {
+                    text: "True",
+                    isCorrect: correctOption === 0,
+                    orderIndex: 0
+                },
+                {
+                    text: "False",
+                    isCorrect: correctOption === 1,
+                    orderIndex: 1
+                }
+            ];
+            console.log("questionType:", questionType);
+            console.log("options:", options);
+        } else if (questionType === 3) {
+            options = [
+                {
+                    text: option1,
+                    isCorrect: false,
+                    orderIndex: 0
+                },
+                {
+                    text: option2,
+                    isCorrect: false,
+                    orderIndex: 1
+                },
+                {
+                    text: option3,
+                    isCorrect: false,
+                    orderIndex: 2
+                },
+                {
+                    text: option4,
+                    isCorrect: false,
+                    orderIndex: 3
+                }
+            ];
+            console.log("questionType:", questionType);
+            console.log("options:", options);
+        }
+
         
         const response = await fetch(`http://localhost:5000/api/quizsets/${id}/questions`,
             {
@@ -105,7 +189,7 @@ function QuizDetailsPage({ accessToken }: QuizDetailsPageProps) {
                 credentials: "include",
                 body: JSON.stringify({
                     text: questionText,
-                    questionType: 0,
+                    questionType,
                     timeLimitSeconds: questionTime,
                     points: questionPoints,
                     options
@@ -132,6 +216,13 @@ function QuizDetailsPage({ accessToken }: QuizDetailsPageProps) {
         setOption4("");
 
         setCorrectOption(0);
+
+        setCorrectOptions([
+            false,
+            false,
+            false,
+            false
+        ]);
     }
 
     async function handleUpdate(event: FormEvent<HTMLFormElement>) {
@@ -235,6 +326,29 @@ function QuizDetailsPage({ accessToken }: QuizDetailsPageProps) {
         setQuestions(data);
     }
 
+    function openQuestion(){
+
+        if (dataForm === false) {
+            setDataForm(true);
+        } else {
+            setDataForm(false);
+        }
+    }
+
+    function showHintText(){
+        if (showHint === false){
+            setShowHint(true);
+        } else {
+            setShowHint(false);
+        }
+    }
+
+    function toggleCorrectOption(index: number){
+        const newCorrectOptions = [...correctOptions];
+        newCorrectOptions[index] = !newCorrectOptions[index];
+        setCorrectOptions(newCorrectOptions);
+    }
+
     useEffect(() => {
         async function loadQuiz() {
             if (!id || !accessToken) {
@@ -308,110 +422,269 @@ function QuizDetailsPage({ accessToken }: QuizDetailsPageProps) {
             </button>
 
             <h2>Вопросы</h2>
-            <form onSubmit={handleCreateQuestion}>
-                <label htmlFor="questionText">Текст вопроса:</label><br />
+            <button onClick={openQuestion}>Добавить новый вопрос</button>
+            <br />
+            
+            {dataForm && (
+                    <form onSubmit={handleCreateQuestion}>
+                    { showHint && (
+                        <p>Подсказка: выбери один правильный вариант ответа</p>
+                    )}
+                    <button type="button" onClick={showHintText}>{ showHint ? "Закрыть подсказку" : "Открыть подсказку"}</button><br />
+                    <br />
+                    <select 
+                        value={questionType}
+                        onChange={(event) => {
+                            setQuestionType(Number(event.target.value))
+                    }}>
+                        
+                        <option value={0}>SingleChoice</option>
+                        <option value={1}>MultipleChoice</option>
+                        <option value={2}>TrueFalse</option>
+                        <option value={3}>Ordering</option>
+                    </select>
+                    <br /><label htmlFor="questionText">Текст вопроса:</label><br />
 
-                <input
-                    id="questionText"
-                    type="text"
-                    value={questionText}
-                    onChange={handleQuestionTextChange}
-                />
+                    <input
+                        id="questionText"
+                        type="text"
+                        value={questionText}
+                        onChange={handleQuestionTextChange}
+                    />
 
-                <br /><br />
+                    <br /><br />
 
-                <label htmlFor="questionPoints">Баллы:</label><br />
+                    <label htmlFor="questionPoints">Баллы:</label><br />
 
-                <input
-                    id="questionPoints"
-                    type="number"
-                    value={questionPoints}
-                    onChange={handleQuestionPointsChange}
-                />
+                    <input
+                        id="questionPoints"
+                        type="number"
+                        value={questionPoints}
+                        onChange={handleQuestionPointsChange}
+                    />
 
-                <br /><br />
+                    <br /><br />
 
-                <label htmlFor="questionTime">Время:</label><br />
+                    <label htmlFor="questionTime">Время:</label><br />
 
-                <input
-                    id="questionTime"
-                    type="number"
-                    value={questionTime}
-                    onChange={handleQuestionTimeChange}
-                />
+                    <input
+                        id="questionTime"
+                        type="number"
+                        value={questionTime}
+                        onChange={handleQuestionTimeChange}
+                    />
 
-                <br /><br />
+                    <br /><br />
 
-                <h3>Варианты ответа</h3>
+                    <h3>Варианты ответа</h3>
 
-                <input
-                    type="text"
-                    placeholder="Ответ 1"
-                    value={option1}
-                    onChange={(event) => setOption1(event.target.value)}
-                />
+                    {questionType === 0 && (
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Ответ 1"
+                                value={option1}
+                                onChange={(event) => setOption1(event.target.value)}
+                            />
 
-                <input
-                    type="radio"
-                    name="correctOption"
-                    checked={correctOption === 0}
-                    onChange={() => setCorrectOption(0)}
-                />
+                            <input
+                                type="radio"
+                                name="correctOption"
+                                checked={correctOption === 0}
+                                onChange={() => setCorrectOption(0)}
+                            />
 
-                <br />
+                            <br />
 
-                <input
-                    type="text"
-                    placeholder="Ответ 2"
-                    value={option2}
-                    onChange={(event) => setOption2(event.target.value)}
-                />
+                            <input
+                                type="text"
+                                placeholder="Ответ 2"
+                                value={option2}
+                                onChange={(event) => setOption2(event.target.value)}
+                            />
 
-                <input
-                    type="radio"
-                    name="correctOption"
-                    checked={correctOption === 1}
-                    onChange={() => setCorrectOption(1)}
-                />
+                            <input
+                                type="radio"
+                                name="correctOption"
+                                checked={correctOption === 1}
+                                onChange={() => setCorrectOption(1)}
+                            />
 
-                <br />
+                            <br />
 
-                <input
-                    type="text"
-                    placeholder="Ответ 3"
-                    value={option3}
-                    onChange={(event) => setOption3(event.target.value)}
-                />
+                            <input
+                                type="text"
+                                placeholder="Ответ 3"
+                                value={option3}
+                                onChange={(event) => setOption3(event.target.value)}
+                            />
 
-                <input
-                    type="radio"
-                    name="correctOption"
-                    checked={correctOption === 2}
-                    onChange={() => setCorrectOption(2)}
-                />
+                            <input
+                                type="radio"
+                                name="correctOption"
+                                checked={correctOption === 2}
+                                onChange={() => setCorrectOption(2)}
+                            />
 
-                <br />
+                            <br />
 
-                <input
-                    type="text"
-                    placeholder="Ответ 4"
-                    value={option4}
-                    onChange={(event) => setOption4(event.target.value)}
-                />
+                            <input
+                                type="text"
+                                placeholder="Ответ 4"
+                                value={option4}
+                                onChange={(event) => setOption4(event.target.value)}
+                            />
 
-                <input
-                    type="radio"
-                    name="correctOption"
-                    checked={correctOption === 3}
-                    onChange={() => setCorrectOption(3)}
-                />
+                            <input
+                                type="radio"
+                                name="correctOption"
+                                checked={correctOption === 3}
+                                onChange={() => setCorrectOption(3)}
+                            />
+                            
 
-                <br /><br />
+                            <br /><br />
+                        </div>
+                    )}
 
-                <button type="submit">
-                    Добавить вопрос
-                </button>
-            </form>
+                    {questionType === 1 && (
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Ответ 1"
+                                value={option1}
+                                onChange={(event) => setOption1(event.target.value)}
+                            />
+
+                            <input
+                                type="checkbox"
+                                name="correctOption"
+                                checked={correctOptions[0]}
+                                onChange={() => toggleCorrectOption(0)}
+                            />
+
+                            <br />
+
+                            <input
+                                type="text"
+                                placeholder="Ответ 2"
+                                value={option2}
+                                onChange={(event) => setOption2(event.target.value)}
+                            />
+
+                            <input
+                                type="checkbox"
+                                name="correctOption"
+                                checked={correctOptions[1]}
+                                onChange={() => toggleCorrectOption(1)}
+                            />
+
+                            <br />
+
+                            <input
+                                type="text"
+                                placeholder="Ответ 3"
+                                value={option3}
+                                onChange={(event) => setOption3(event.target.value)}
+                            />
+
+                            <input
+                                type="checkbox"
+                                name="correctOption"
+                                checked={correctOptions[2]}
+                                onChange={() => toggleCorrectOption(2)}
+                            />
+
+                            <br />
+
+                            <input
+                                type="text"
+                                placeholder="Ответ 4"
+                                value={option4}
+                                onChange={(event) => setOption4(event.target.value)}
+                            />
+
+                            <input
+                                type="checkbox"
+                                name="correctOption"
+                                checked={correctOptions[3]}
+                                onChange={() => toggleCorrectOption(3)}
+                            />
+                            
+
+                            <br /><br />
+                        </div>
+                    )}
+                    { questionType === 2 && (
+                       <div>
+                            <input 
+                                id="trueOption"
+                                type="radio"
+                                name="correctOption"
+                                checked={correctOption === 0}
+                                onChange={() => setCorrectOption(0)}
+                            /> 
+                            <label htmlFor="trueOption">Правда</label> <br />
+                            <input 
+                                id="falseOption"
+                                type="radio"
+                                name="correctOption"
+                                checked={correctOption === 1}
+                                onChange={() => setCorrectOption(1)}
+                            /> 
+                            <label htmlFor="falseOption">Ложь</label>
+                       </div> 
+                    )}
+
+                    {questionType === 3 && (
+                        <div>
+                            <label htmlFor="orderOption1">1.</label>
+                            <input
+                                id="orderOption1"
+                                type="text"
+                                value={option1}
+                                onChange={(event) => setOption1(event.target.value)}
+                            />
+
+                            <br />
+
+                            <label htmlFor="orderOption2">2.</label>
+                            <input
+                                id="orderOption2"
+                                type="text"
+                                value={option2}
+                                onChange={(event) => setOption2(event.target.value)}
+                            />
+
+                            <br />
+
+                            <label htmlFor="orderOption3">3.</label>
+                            <input
+                                id="orderOption3"
+                                type="text"
+                                value={option3}
+                                onChange={(event) => setOption3(event.target.value)}
+                            />
+
+                            <br />
+
+                            <label htmlFor="orderOption4">4.</label>
+                            <input
+                                id="orderOption4"
+                                type="text"
+                                value={option4}
+                                onChange={(event) => setOption4(event.target.value)}
+                            />
+                        </div>
+                    )}
+
+
+                    <button type="submit">
+                        Добавить вопрос
+                    </button>
+                </form>
+            )}
+
 
             {questions.length === 0 && (
                 <p>Вопросов пока нет.</p>
